165a9f016999dcdea880f2fa00552ae5adf3b2f7
Sameh Selim <sselim@t-online.de>
2020-12-15 22:15:18 +0100

    User Data demo

--- example.tf
+++ example.tf
@@ -18,6 +18,10 @@ provider "aws" {
   region  = var.aws_region
 }
 
+data "aws_security_group" dflt_sg {
+  name = "default"
+}
+
 resource "aws_security_group" "ec2_ssh" {
   name = "ec2_ssh_sg"
   ingress {
@@ -27,22 +31,26 @@ resource "aws_security_group" "ec2_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 8080
+    from_port = 80
     protocol = "tcp"
-    to_port = 8080
+    to_port = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "example" {
-  ami             = "ami-08d70e59c07c61a3a" #Amazon Linux 2/us-west-2
+ # ami             = "ami-08d70e59c07c61a3a" #ubuntu/us-west-2
+  ami             = "ami-0e472933a1395e172" #Amazon Linux 2/us-west-2
   instance_type   = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.ec2_ssh.id]
+  vpc_security_group_ids = [aws_security_group.ec2_ssh.id,data.aws_security_group.dflt_sg.id]
   key_name        = aws_key_pair.ec2_ssh_key.key_name
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello World from $(hostname -f)" > index.html
-              nohup busybox httpd -f -p 8080 &
+              yum -y update
+              yum -y install httpd.x86_64
+              systemctl start httpd.service
+              systemctl enable https.service
+              echo "Hello World from $(hostname -f)" >> /var/www/html/index.html
               EOF
 }
 
